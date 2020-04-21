9203b7bb70d7f1b97f37a523f2dd3bcca3d61d02
nic <17612158006@163.com>
2020-04-21 21:50:40 +0800

    first commit

---
+++ oauth2-server/src/main/java/com/nic/cloud/demo/oauth2/server/Oauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.nic.cloud.demo.oauth2.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author james
+ * @date 2020/4/20 21:36
+ */
+@SpringBootApplication
+public class Oauth2ServerApplication {
+}
