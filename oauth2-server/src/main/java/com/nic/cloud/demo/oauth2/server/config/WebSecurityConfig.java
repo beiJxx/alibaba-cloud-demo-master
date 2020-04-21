b3e8b55c1c6bfa84f7b6cfde15a8f321381ef3c5
nic <17612158006@163.com>
2020-04-21 22:04:03 +0800

    test commit

---
+++ oauth2-server/src/main/java/com/nic/cloud/demo/oauth2/server/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.nic.cloud.demo.oauth2.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Description:
+ *
+ * @author james
+ * @date 2020/4/21 22:02
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+}
